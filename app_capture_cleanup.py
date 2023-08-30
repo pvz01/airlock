1eb949461fa2f56f0c9bbe3d12caa1e9290bfd82
Patrick Van Zandt <81989793+pvz01@users.noreply.github.com>
2023-08-30 11:49:26 -0500

    Update app_capture_cleanup.py

--- app_capture_cleanup.py
+++ app_capture_cleanup.py
@@ -1,7 +1,10 @@
-# Example showing how to clean up unwanted data in existing app captures,
-# using the example of "remove everything with a Python extension".
+# Example showing how to clean up unwanted data in existing app captures, using the 
+# example of "remove everything with a Python extension".
+#
+# This script inspects all application captures on the server, identifies if changes are
+# warranted, and if yes prompts for confirmation before making changes.
 
-#required libraries
+#import required libraries
 import requests
 import json
 import xml.etree.ElementTree as ET
@@ -15,67 +18,75 @@ headers = {'X-APIKey': 'api-key-here'}
 #set the list of file extensions for files that you want to remove
 extensions_to_remove = ['.py', '.pyi', '.pyc', '.pyd', '.pyo', '.pyw', '.pyz']
 
-#set True/False whether to prompt user before making changes
+#set True/False whether to prompt for each application before making changes
 safety_check = True
 
-#get list of appplications
+#get list of applications
 request_url = base_url + '/v1/application'
 response = requests.post(request_url, headers=headers, verify=False)
-print(response.status_code, request_url)
 applications = response.json()['response']['applications']
-print('Found', len(applications), 'applications')
+print('INFO: Found', len(applications), 'applications')
 
-#iterate through the applications
+#iterate through the application list
 for application in applications:
+	print('\nINFO: Processing', application['name'])
+	
+	#get the application
+	request_url = base_url + '/v1/application/export'
+	payload = {'applicationid': application['applicationid']}
+	response = requests.post(request_url, headers=headers, json=payload, verify=False)
+	xml_content = response.text
+	
+	print('INFO: Downloaded application from server. Beginning analysis.')
+	
+	#create lists to store hashes
+	sha256_remove = []
+	sha256_keep = [] 
 
-    print('\nProcessing application', application['name'])
-    
-    #get the application
-    request_url = base_url + '/v1/application/export'
-    payload = {'applicationid': application['applicationid']}
-    response = requests.post(request_url, headers=headers, json=payload, verify=False)
-    print(response.status_code, request_url)
-    xml_content = response.text    
+	#parse the application xml
+	root = ET.fromstring(xml_content)
+	results_section = root.find(".//ResultsSection")
+	if results_section is not None:
+		for result in results_section.findall('fileload'):
+			path_element = result.find('path')
+			if path_element is not None:
+				path = path_element.text
+				sha256_element = result.find('sha256')
+				if sha256_element is not None:
+					sha256_value = sha256_element.text
+					#check if the path ends with one of the configured extensions
+					if any(path.endswith(extension) for extension in extensions_to_remove):
+						if sha256_value not in sha256_remove:
+							#hash to be removed has been found, add it to list
+							sha256_remove.append(sha256_value)
+					else:
+						if sha256_value not in sha256_keep:
+							#hash to be kept has been found, add it to list
+							sha256_keep.append(sha256_value)
 
-    #parse the XML content, generating lists of unique sha256 values
-    sha256_keep = []
-    sha256_remove = []
-
-    root = ET.fromstring(xml_content)
-    results_section = root.find(".//ResultsSection")
-
-    if results_section is not None:
-        for result in results_section.findall('fileload'):
-            path_element = result.find('path')
-            if path_element is not None:
-                path = path_element.text
-                sha256_element = result.find('sha256')
-                if sha256_element is not None:
-                    sha256_value = sha256_element.text
-                    if any(path.endswith(extension) for extension in extensions_to_remove):
-                        if sha256_value not in sha256_remove:
-                            sha256_remove.append(sha256_value)
-                    else:
-                        if sha256_value not in sha256_keep:
-                            sha256_keep.append(sha256_value)
-
-    print('Identified', len(sha256_remove), 'files to be removed from', application['name'], 'based on having one of these extensions:', extensions_to_remove)
-    print('This application also contains', len(sha256_keep), 'other files, which will not be removed.')
-    
-    if len(sha256_remove) > 0:
-        
-        if safety_check:
-            user_response = input('Do you want to remove the matching files? Enter YES or NO: ')
-            if user_response.lower() != 'yes':
-                print('Skipping additional processing of application', application['name'], 'based on your reponse', user_response)
-                continue
-                
-        print('Removing', len(sha256_remove), 'hashes from', application['name'])
-        
-        request_url = base_url + '/v1/hash/application/remove'
-        payload = {'applicationid': application['applicationid'],
-                   'hashes': sha256_remove}
-        response = requests.post(request_url, headers=headers, json=payload, verify=False)
-        print(response.status_code, request_url, payload)
-    
-    print('\nDone processing application', application['name'])
+	#print summary of application analysis
+	print('INFO: Analysis is complete. Found', (len(sha256_remove) + len(sha256_keep)), 'total unique hashes.')
+	print('INFO:', len(sha256_remove), 'of those match search criteria', extensions_to_remove, '.')
+	print('INFO:', len(sha256_keep), 'of those do not match search criteria.')
+	
+	#if there is nothing to remove, print message and exit this iteration of for loop
+	if len(sha256_remove) == 0:
+		print('INFO: Because no matches were found, taking no further action on this application.')
+		continue  #go to next application
+		
+	#if there is nothing to keep, print warning but proceed
+	elif len(sha256_keep) == 0:
+		print('WARNING: All files match the search criteria. Removal will result in an empty application.')
+	
+	#if safety_check is enabled, prompt user and if they answer anything except yes then exit this iteration of for loop
+	if safety_check:
+		user_response = input(f'Do you want to remove {len(sha256_remove)} files from {application["name"]} [YES | NO]? ')
+		if user_response.lower() != 'yes':
+			continue  #go to next application
+	   
+	#execute the removal
+	print('INFO: Removing', len(sha256_remove), 'hashes from', application['name'])
+	request_url = base_url + '/v1/hash/application/remove'
+	payload = {'applicationid': application['applicationid'],
+			   'hashes': sha256_remove}
+	response = requests.post(request_url, headers=headers, json=payload, verify=False)
