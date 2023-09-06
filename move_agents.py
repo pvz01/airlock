b276e93971a661cae15e67a68acff5a86290ae5e
Patrick Van Zandt <81989793+pvz01@users.noreply.github.com>
2023-09-06 13:40:52 -0500

    Update move_agents.py

--- move_agents.py
+++ move_agents.py
@@ -3,27 +3,17 @@
 # Use this command to install prerequisites:
 #     pip install requests
 
-import requests, json, sys
+#required libraries for interacting with REST API
+import requests
+import json
+import sys
 
-
-#prompt for config
-server_fqdn = input('Enter server fqdn: ')
-server_port = input('Enter server port, or press return to accept the default (3129): ')
-if server_port == '':
-	server_port = 3129
-api_key = input('Enter API key: ')
-filename = input('Create a plain text file with one hostname per line. Enter name of that file here, or press return to accept the default (hostnames.txt): ')
+#prompt for configuration
+server_fqdn = input('Server fqdn: ')
+api_key = input('API key: ')
+filename = input('Create a plain text file with one hostname per line and place it in the working directory that you ran this script from. Enter name of that file here, or press return to accept the default (hostnames.txt): ')
 if filename == '':
 	filename = 'hostnames.txt'
-
-#option to disable SSL certificate verification when running against a non-prod server
-is_lab_server = False
-if is_lab_server:
-	verify_ssl = False
-	import urllib3
-	urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-else:
-	verify_ssl = True
 
 #read list of hostnames from disk
 hostnames = []
@@ -38,46 +28,46 @@ print('INFO: Read', len(hostnames), 'hostnames from', filename)
 print(hostnames)
 
 #calculate base configuration used for requests to server
-base_url = 'https://' + server_fqdn + ':' + str(server_port) + '/'
+base_url = 'https://' + server_fqdn + ':3129/'
 headers = {'X-APIKey': api_key}
 
-#get list of groups
+#read group list
 request_url = f'{base_url}v1/group'
-response = requests.post(request_url, headers=headers, verify=verify_ssl)
+response = requests.post(request_url, headers=headers, verify=False)
 if response.status_code != 200:
 	print('ERROR: Unexpected return code', response.status_code, 'on HTTP POST', request_url, 'with headers', headers)
 	sys.exit(0)
 groups = response.json()['response']['groups']
 print('INFO: Found', len(groups), 'groups on the server')
 
-#ask user which group to move the device(s) to
+#prompt for group selection
 for index, item in enumerate(groups):
-	print(f'{index}: {item["name"]} ({item["groupid"]})')
-index = int(input('Which group do you want to move the agents to? '))
+    print(f'{index+1}: {item["name"]} ({item["groupid"]})')
+user_input = input('Which group do you want to move the agents to? ')
+index = int(user_input)-1
 group = groups[index]
 print('INFO: You chose', group)
 
-#sanity check
+#prompt for sanity check
 proceed = input('Are you sure you want to move ' + str(len(hostnames)) + ' agents to the group "' + group['name'] + '"? Enter YES to proceed: ')
 if proceed.lower() != 'yes':
 	sys.exit(0)
 
+#perform the moves
 print('Attempting to move', len(hostnames), 'devices to the group', group['name'])
 
-#establish counters and lists to track results
 counter = 1
 successes = []
 failures = []
 
-#iterate through the hostnames
 for hostname in hostnames:
 	print(counter, '/', len(hostnames))
 
-	#find the agent id of the hostname
+	#find agent id(s)
 	print('INFO: Searching for agent id for hostname', hostname)
 	request_url = f'{base_url}v1/agent/find'
 	payload = {'hostname': hostname}
-	response = requests.post(request_url, headers=headers, verify=verify_ssl, json=payload)
+	response = requests.post(request_url, headers=headers, json=payload, verify=False)
 	if response.status_code != 200:
 		print('ERROR: Unexpected return code', response.status_code, 'on HTTP POST', request_url, 'with headers', headers, 'and payload', payload)
 		sys.exit(0)
@@ -92,11 +82,12 @@ for hostname in hostnames:
 		for agent in agents:
 			agentid = agent['agentid']
 			print('INFO: Found a match for', hostname, 'with agent id', agentid)
-			#perform the move
+			
+			#execute the move
 			request_url = f'{base_url}v1/agent/move'
 			payload = {'agentid': agentid, 'groupid': group['groupid']}
 			print('INFO: Moving agent', hostname , 'with GUID', agentid, 'to group', group['name'], 'with GUID', group['groupid'])
-			response = requests.post(request_url, headers=headers, verify=verify_ssl, json=payload)
+			response = requests.post(request_url, headers=headers, json=payload, verify=False)
 			if response.status_code == 200:
 				print('Success')
 				successes.append(hostname)
@@ -105,7 +96,7 @@ for hostname in hostnames:
 				failures.append(hostname)
 	counter += 1
 
-#print results to console
+#print results
 print('\nSuccessfully moved', len(successes), 'devices to', group['name'], group['groupid'])
 for hostname in successes:
 	print(hostname)
