2b95b26bdffc985a2f7c04cbfa1ee53a0c3a4506
Patrick Van Zandt <81989793+pvz01@users.noreply.github.com>
2023-11-02 11:35:20 -0500

    Add files via upload

---
+++ export_server_activity.py
@@ -0,0 +1,50 @@
+# Example of how to export Server Activity History Logs to disk in CSV format
+# 
+# Use this command to install prerequisites:
+#     pip install requests datetime
+
+
+##CONFIGURATION
+
+# Server configuration
+server_fqdn = 'your-server-name'
+apikey = 'your-api-kay'
+
+# Checkpoint
+# To export new data, provide checkpoint from the last row of previous export
+# To export all data, set to '000000000000000000000000'
+checkpoint = '000000000000000000000000'
+
+
+##RUNTIME
+
+# Import required libraries
+import requests
+import json
+import csv
+import datetime
+import dateutil
+
+# Suppress SSL warnings
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Get data from server
+request_url = f'https://{server_fqdn}:3129/v1/logging/svractivities'
+request_headers = {'X-APIKey': apikey}
+request_body = {'checkpoint': checkpoint}
+response = requests.post(request_url, headers=request_headers, json=request_body, verify=False)
+svractivities = response.json()['response']['svractivities']
+
+# Calculate file name for export
+servername = server_fqdn.replace(".", "-")
+timestamp = datetime.datetime.today().strftime("%Y-%m-%d_%H.%M")
+file_name = f'airlock_server_activity_{servername}_{timestamp}.csv'
+
+# Write data to disk
+with open(file_name, mode='w', newline='') as csv_file:
+    fieldnames = svractivities[0].keys()
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(svractivities)
+print(len(svractivities), 'records written to', file_name)
