473fc5eeea0d7c4be73af90af0ef49913ddbabb2
Patrick Van Zandt <81989793+pvz01@users.noreply.github.com>
2023-11-02 18:33:33 -0500

    Added pagination support to export_server_activity.py
    Added logic to allow export of >10K rows of data in a single run

--- export_server_activity.py
+++ export_server_activity.py
@@ -1,7 +1,7 @@
 # Example of how to export Server Activity History Logs to disk in CSV format
 # 
 # Use this command to install prerequisites:
-#     pip install requests datetime
+#	  pip install requests datetime
 
 
 ##CONFIGURATION
@@ -31,9 +31,19 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 # Get data from server
 request_url = f'https://{server_fqdn}:3129/v1/logging/svractivities'
 request_headers = {'X-APIKey': apikey}
-request_body = {'checkpoint': checkpoint}
-response = requests.post(request_url, headers=request_headers, json=request_body, verify=False)
-svractivities = response.json()['response']['svractivities']
+collected_data = []
+while True:
+	request_body = {'checkpoint': checkpoint}
+	print('Querying server for entries after checkpoint', checkpoint)
+	response = requests.post(request_url, headers=request_headers, json=request_body, verify=False)
+	svractivities = response.json()['response']['svractivities']
+	checkpoint = svractivities[len(svractivities)-1]['checkpoint']
+	print('Found', len(svractivities), 'rows of data, the last of which has checkpoint', checkpoint) 
+	collected_data += svractivities
+	if len(svractivities) < 10000:
+		# We have all the data. Exit the while loop.
+		break
+print(len(collected_data), 'total rows of data downloaded from server')
 
 # Calculate file name for export
 servername = server_fqdn.replace(".", "-")
@@ -41,9 +51,10 @@ timestamp = datetime.datetime.today().strftime("%Y-%m-%d_%H.%M")
 file_name = f'airlock_server_activity_{servername}_{timestamp}.csv'
 
 # Write data to disk
+print('Beginning export of data to', file_name)
 with open(file_name, mode='w', newline='') as csv_file:
-    fieldnames = svractivities[0].keys()
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    writer.writeheader()
-    writer.writerows(svractivities)
-print(len(svractivities), 'records written to', file_name)
+	fieldnames = collected_data[0].keys()
+	writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+	writer.writeheader()
+	writer.writerows(collected_data)
+print(len(collected_data), 'records written to', file_name)
